daddb5f460dbe9fec624e4d80edfdcaaba5a14d1
조해창 <outofchng@gmail.com>
2024-06-03 22:23:07 +0900

    타이포 수정

--- backend/app/services/help.py
+++ backend/app/services/help.py
@@ -3,11 +3,11 @@ from app.utils import kakao_json_response
 
 def create_help_center_response():
     kakao_response = kakao_json_response.KakaoJsonResponse()
-    return kakao_response.add_output_to_response(
+    kakao_response.add_output_to_response(
         {
             "textCard": kakao_response.create_text_card(
                 title="츠누봇을 함께 만들어가요 👊",
-                description="• 잘못된 정보가 있나요?\n\n• 새로운 기능이 있으면 필요한가요?",
+                description="• 잘못된 정보가 있나요?\n\n• 새로운 기능이 필요한가요?",
                 buttons=[
                     {
                         "action": "operator",
@@ -17,3 +17,4 @@ def create_help_center_response():
             )
         }
     )
+    return kakao_response.get_response()
